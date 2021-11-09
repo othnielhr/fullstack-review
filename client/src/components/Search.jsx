c2f7e861c4c3b632c44fd753827bb97b9adbeaf6
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-09 15:44:04 -0600

    diagram and schema approved by Dominic M

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -9,6 +9,8 @@ class Search extends React.Component {
   }
 
   onChange (e) {
+    console.log('onChange', e.target.value)
+    console.log('state', this)
     this.setState({
       term: e.target.value
     });
@@ -21,9 +23,9 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
