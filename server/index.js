1cadfbe88fe29c84a66577ae0cbd5c4e439d5793
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-10 14:27:58 -0600

    Complete getReposByUsername

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 let app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const getReposByUsername = require('../helpers/github.js').getReposByUsername;
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
@@ -11,10 +12,17 @@ app.post('/repos', function (req, res) {
   // and get the repo information from the github API, then
   // save the repo information in the database
   // console.log('req body', req.body);
-  var username = req.body.term
+  let username = req.body.term;
   // console.log(username);
   // use axios to send a get request to github api
-  res.sendStatus(200);
+  getReposByUsername(username, (resData) => {
+    // console.log('res data', resData);
+    // let data = JSON.parse(resData);
+    resData.forEach((repo) => {
+      let repoObj = {forks: repo.forks, username: repo.owner.login, repo_url: repo.html_url, repo_name: repo.name }
+      console.log(repoObj);
+    });
+  })
 });
 
 app.get('/repos', function (req, res) {
