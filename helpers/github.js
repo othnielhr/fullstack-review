f83324c517ca6eaa84693a418b2c091b9df94d52
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-10 11:34:43 -0600

    starting axios get request for github api

--- helpers/github.js
+++ helpers/github.js
@@ -4,11 +4,12 @@ const config = require('../config.js');
 let getReposByUsername = (/* TODO */) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
+  // use axios.get
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: 'http://api.github.com',
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
