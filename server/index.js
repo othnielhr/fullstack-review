2d3cdbb90c098eec21d2e7fe534f6056af5acbef
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-09 21:16:23 -0600

    implemented post request to server

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
-  console.log('server side');
-  console.log('req body', req);
+  // console.log('req body', req.body);
+  var username = req.body.term
+  // console.log(username);
+  // use axios to send a get request to github api
   res.sendStatus(200);
 });
 
