338fa9af3d2b13add3aeaa75c75794975be40a91
Othniel Chan <othnielhr.chan@gmail.com>
2021-11-10 20:34:28 -0600

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
 .then(() => console.log('connected to db'))
 .catch(err => console.log(err));
 
@@ -7,8 +7,8 @@ let repoSchema = mongoose.Schema({
   // TODO: your schema here!
   // id: Number, // add your own unique id {unique: true}
   username: String, //data.owner.login
-  repo: String, // data.name to make unique {type: String, unieuq: true}
-  url: String, // data.html_url
+  repo: String, // data.name
+  url: {type: String, unique: true}, // data.html_url changed this key to be unique instead because some users may have same repo names but full url should be different
   forks: Number // data.forks
 });
 // create Repo model
@@ -29,7 +29,8 @@ let save = (repoData) => {
 
   repo.save(function(err, data) {
     if(err) {
-      console.log('error', err);
+      // err sent when a duplicate repo url is found
+      console.log('error dupe');
     } else {
       console.log('saved');
     }
